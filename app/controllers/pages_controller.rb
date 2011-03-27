fc025b6649adf3f1acda75c5f8e3d756dfa1fd87
Clay Yearsley <clayyearsley@yahoo.com>
2011-03-27 18:19:12 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home (embedded)"
   end
 
   def contact
+  	@title = "Contact"
+  end
+  
+  def about
+  	@title = "About"
   end
 
 end
